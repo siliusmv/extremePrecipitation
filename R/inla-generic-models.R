2f61624f59bf4df5f3b44fc131d21ea3349117bc
Silius M. Vandeskog <siliusv@gmail.com>
2023-06-23 08:25:42 +0200

    Add new spde_b_model where beta0 cannot exceed 1

--- R/inla-generic-models.R
+++ R/inla-generic-models.R
@@ -103,6 +103,70 @@ spde_b_model = function(n,
   do.call(INLA::inla.cgeneric.define, args)
 }
 
+#' @export
+spde_b_model2 = function(n,
+                         y0,
+                         spde,
+                         init,
+                         priors,
+                         dist_to_s0,
+                         is_fixed = rep(FALSE, n_theta),
+                         debug = FALSE) {
+  n_theta = 5
+  if (!all(class(spde) == "list")) spde = list(spde)
+  stopifnot(length(n) == length(spde))
+  stopifnot(length(n) == length(dist_to_s0))
+  stopifnot(sum(n) == length(y0))
+  stopifnot(all(sapply(spde, `[[`, "n.spde") == sapply(dist_to_s0, length)))
+  stopifnot(is.logical(is_fixed))
+  stopifnot(length(init) == n_theta)
+  stopifnot(length(is_fixed) == length(init))
+  stopifnot(all(c(
+    "rho", "sigma", "beta0", "lambda", "kappa")[!is_fixed] %in% names(priors)))
+  stopifnot(all(sapply(priors, length) == 2))
+  stopifnot(all(sapply(dist_to_s0, min) == 0))
+
+  # Locate and remove the s0_locs from dist_to_s0
+  s0_location = sapply(dist_to_s0, function(x) which(x == 0))
+  stopifnot(is.integer(s0_location))
+  for (i in seq_along(dist_to_s0)) dist_to_s0[[i]] = dist_to_s0[[i]][-s0_location[i]]
+
+  # Start building the argument list for the cgeneric model
+  args = list(debug = debug)
+
+  # The name and location of the required c-function for defining the cgeneric model
+  args$model = "spde_b_model2"
+  args$shlib = file.path(cgeneric_dir(), "b.so")
+
+  # Put all the arguments into args in the order defined in the c-function
+  args$n = sum(sapply(dist_to_s0, length) * n)
+  args$s0_index = rep(seq_along(n), n) - 1L # 0-indexation in C
+  args$is_fixed = as.integer(is_fixed)
+  args$s0_location = s0_location - 1L # 0-indexation in C
+  args$init = init
+  args$y0 = y0
+  if (!is_fixed[1]) args$rho_prior = priors$rho
+  if (!is_fixed[2]) args$sigma_prior = priors$sigma
+  if (!is_fixed[3]) args$beta0_prior = priors$beta0
+  if (!is_fixed[4]) args$lambda_prior = priors$lambda
+  if (!is_fixed[5]) args$kappa_prior = priors$kappa
+
+  for (i in seq_along(n)) {
+    args[[paste0("dist_to_s0_", i)]] = dist_to_s0[[i]]
+    args[[paste0("B0_", i)]] = spde[[i]]$param.inla$B0
+    args[[paste0("B1_", i)]] = spde[[i]]$param.inla$B1
+    args[[paste0("B2_", i)]] = spde[[i]]$param.inla$B2
+    args[[paste0("M0_", i)]] = spde[[i]]$param.inla$M0
+    args[[paste0("M1_", i)]] = spde[[i]]$param.inla$M1
+    args[[paste0("M2_", i)]] = spde[[i]]$param.inla$M2
+  }
+
+  # Define the model
+  do.call(INLA::inla.cgeneric.define, args)
+}
+
+
+
 #' spdes: a list of M spdes
 #' priors: a list of PC priors for the range and standard deviation parameters
 #' init: initial values for the range and standard deviation parameters
